f69cc6f4e83b0c6623fb844661b5a94c8f4ffc9a
Jean Piere <j.sanchez@creatienda.cl>
2024-12-21 03:21:38 -0500

    Add Filament Admin Panel Provider and related components

---
+++ app/Filament/Pages/Logs.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Actions\Action;
+use Filament\Pages\Page;
+use Filament\Widgets\StatsOverviewWidget;
+use Illuminate\Support\Facades\Http;
+use Laravel\Forge\Forge;
+
+class Logs extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.logs';
+    protected static ?string $title = 'Logs';
+
+    public $defaultAction = 'onboarding';
+
+    public $forge = null;
+    public $logs = '';
+    public $selectedProject;
+    public $projects = [];
+    public $selectedServer;
+    public $servers = [];
+    public $selectedSite;
+    public $sites = [];
+    public $selectedDeployment;
+    public $deployments = [];
+
+    public function mount()
+    {
+        $this->loadServers();
+    }
+
+    protected function getForgeInstance()
+    {
+        return $this->forge ?? new Forge(env('FORGE_API_TOKEN'));
+    }
+
+    protected function loadServers()
+    {
+        $forge = $this->getForgeInstance();
+
+        $this->servers = collect($forge->servers())->map(function ($server) {
+            return [
+                'id' => $server->id,
+                'name' => $server->name,
+            ];
+        })->toArray();
+    }
+
+    public function loadSites()
+    {
+        if ($this->selectedServer) {
+            $forge = $this->getForgeInstance();
+            $this->sites = collect($forge->sites($this->selectedServer))->map(function ($site) {
+                return [
+                    'id' => $site->id,
+                    'name' => $site->name,
+                    'serverId' => $site->serverId,
+                ];
+            })->toArray();
+        }
+
+        //reset deployments and logs
+        $this->selectedSite = null;
+        $this->deployments = [];
+        $this->selectedDeployment = null;
+        $this->logs = '';
+    }
+
+    public function loadDeployments()
+    {
+        if ($this->selectedServer && $this->selectedSite) {
+            $forge = $this->getForgeInstance();
+            $site = $forge->site($this->selectedServer, $this->selectedSite);
+            $this->deployments = collect($site->getDeploymentHistory()['deployments'])->map(function ($deployment) {
+                return [
+                    'id' => $deployment['id'],
+                    'name' => $deployment['commit_message'],
+                    'started_at' => $deployment['started_at'],
+                ];
+            })->toArray();
+        }
+
+        // reset 
+        $this->logs = '';
+        $this->selectedDeployment = null;
+    }
+
+    public function loadLogs()
+    {
+        if ($this->selectedServer && $this->selectedSite && $this->selectedDeployment) {
+            $forge = $this->getForgeInstance();
+            $site = $forge->site($this->selectedServer, $this->selectedSite);
+            try {
+                $output = $site->getDeploymentHistoryOutput($this->selectedDeployment)['output'] ?? 'No Se puedo cargar los logs';
+                $this->logs = $this->removeAnsiSequences($output);
+            } catch (\Laravel\Forge\Exceptions\NotFoundException $e) {
+                $this->logs = 'No Se puedo cargar los logs: ' . $e->getMessage();
+            }
+        }
+    }
+
+    function removeAnsiSequences($text) {
+        if ($text === null) {
+            return '';
+        }
+        // Regex para eliminar secuencias ANSI
+        $text = preg_replace('/\x1B\[[0-9;]*m/', '', $text);
+        return $text;
+    }
+
+    public function onboardingAction(): Action
+    {
+        return Action::make('onboarding')
+            ->modalHeading('Welcome Che')
+            ->visible(fn (): bool => false);
+    }
+ 
+    // protected function getHeaderWidgets(): array
+    // {
+    //     return [
+    //         StatsOverviewWidget::class
+    //     ];
+    // }
+
+    public static function canAccess(): bool
+    {
+        return true; //auth()->user()->can('view logs');
+    }
+
+    // public function deleteAction(): Action
+    // {
+    //     return Action::make('delete')
+    //         ->requiresConfirmation()
+    //         ->action(fn (array $arguments) => dd($arguments['something']));
+    // }
+
+    // public function testAction(): Action
+    // {
+    //     return Action::make('test')
+    //         ->requiresConfirmation()
+    //         ->action(function (array $arguments) {
+    //             dd('Test action called', $arguments);
+    //         });
+    // }
+
+    // protected function getHeaderActions(): array
+    // {
+    //     return [
+    //         Action::make('edit')
+    //             ->url('logs/{record}/edit'),
+    //         Action::make('delete')
+    //             ->requiresConfirmation()
+    //             ->action(fn () => dd('Delete action called')),
+    //     ];
+    // }
+}
---
+++ resources/views/filament/pages/logs.blade.php
@@ -0,0 +1,52 @@
+<x-filament-panels::page>
+    {{-- {{ ($this->deleteAction)(['something' => 'BRO'])}} --}}
+
+    {{-- <button wire:click="mountAction('test', { id: 12345 })">
+        Button
+    </button> --}}
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <x-filament::input.wrapper class="p-2">
+            <div class="flex justify-between">
+                <label for="server">Server:</label>
+                <x-filament::loading-indicator wire:loading wire:target="loadSites" class="text-right h-5 w-5" />
+            </div>
+            <x-filament::input.select id="server" wire:model="selectedServer" wire:change="loadSites">
+                <option value="">Select Server</option>
+                @foreach($servers as $server)
+                    <option value="{{ $server['id'] }}">{{ $server['name'] }}</option>
+                @endforeach
+            </x-filament::input.select>
+        </x-filament::input.wrapper>
+
+        <x-filament::input.wrapper class="p-2">
+            <div class="flex justify-between">
+                <label for="site">Site:</label>
+                <x-filament::loading-indicator wire:loading wire:target="loadDeployments" class="text-right h-5 w-5" />
+            </div>
+            <x-filament::input.select id="site" wire:model="selectedSite" wire:change="loadDeployments">
+                <option value="">Select Site</option>
+                @foreach($sites as $site)
+                    <option value="{{ $site['id'] }}">{{ $site['name'] }}</option>
+                @endforeach
+            </x-filament::input.select>
+        </x-filament::input.wrapper>
+
+        <x-filament::input.wrapper class="p-2">
+            <div class="flex justify-between">
+                <label for="deployment">Deployment:</label>
+                <x-filament::loading-indicator wire:loading wire:target="loadLogs" class="text-right h-5 w-5" />
+            </div>
+            <x-filament::input.select id="deployment" wire:model="selectedDeployment" wire:change="loadLogs">
+                <option value="">Select Deployment</option>
+                @foreach($deployments as $deployment)
+                    <option value="{{ $deployment['id'] }}">{{ $deployment['started_at'] }}</option>
+                @endforeach
+            </x-filament::input.select>
+        </x-filament::input.wrapper>
+    </div>
+
+    <textarea readonly style="background-color: black; color: white; font-family: monospace; padding: 10px; border-radius: 5px; white-space: pre; width: 100%; height: 400px; border: none; overflow: auto; resize: vertical;">
+        {!! $logs !!}
+    </textarea>
+</x-filament-panels::page>
