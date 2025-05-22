96de70a24fbaaadf78601a4182f82f85a43f32ac
Jean Piere <j.sanchez@creatienda.cl>
2025-05-22 10:21:56 -0500

    Remove filtering of servers to include only 'creatienda-qa' in loadServers method

--- app/Filament/Pages/Logs.php
+++ app/Filament/Pages/Logs.php
@@ -60,9 +60,9 @@ class Logs extends Page
         })->toArray();
 
         // solo dejar el que tenga name == 'creatienda-qa'
-        $this->servers = collect($this->servers)->filter(function ($server) {
-            return $server['name'] == 'creatienda-qa';
-        })->toArray();
+        // $this->servers = collect($this->servers)->filter(function ($server) {
+        //     return $server['name'] == 'creatienda-qa';
+        // })->toArray();
     }
 
     public function loadSites()
